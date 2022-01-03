f5bbacf8bd499aae412448259a69a1e815cf7860
david weinstein <88619278+464david464@users.noreply.github.com>
2022-01-04 00:22:12 +0200

    add

---
+++ even & odd/js/nain.js
@@ -0,0 +1,8 @@
+// even or odd number test ========================
+const num = parseInt(prompt('enter any number'));
+if((num % 2) == 0){
+    alert('even')
+} else {
+    alert('odd')
+};
+//===================================================
